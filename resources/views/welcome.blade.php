c400f72241cbf1aca72d720bb89f59bff5531a6d
Parkboyoung11 <vuhongsonjv11@gmail.com>
2020-08-10 11:31:31 +0700

    Move JS CSS to public

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,73 +1,22 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{str_replace('_', '-', app()->getLocale())}}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="icon" href="{{ asset('images/icon.png') }}">
+        <link rel="icon" href="{{asset('images/icon.png')}}">
         <title>Aki Shorten Url</title>
 
         <!-- Fonts -->
-        <link href="{{ asset('font/font') }}" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+        <link href="{{asset('font/font') }" rel="stylesheet" type="text/css">
+
+        <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{asset('css/custom.css')}}">
+
         <script src="{{ asset('js/jquery-3.2.1.slim.min.js') }}"></script>
         <script src="{{ asset('js/popper.min.js') }}"></script>
         <script src="{{ asset('js/jquery.min.js') }}"></script>
 	    <script src="{{ asset('js/jquery.bootstrap-growl.js') }}"></script>
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #000;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-                margin-top: 10%;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
         <div class="container">
@@ -87,60 +36,6 @@
             </div>
         </div>
     </body>
-    <script type="text/javascript">
-        $("#shorten").on('click', function(){
-            if($("#originLink").val() !== "" &&  $("#shorten").text() == "SHORTEN" ) {
-                $("#shorten").text("Loading...");
-                var originLink = $("#originLink").val();
-                shortenUrl(originLink);
-            }
-        })
-
-        function shortenUrl(originLink) {
-            $.ajax({
-                type: 'POST',
-                url: 'http://akii.tk/api.php',
-                data: {
-                    key: '{{ env('USER_KEY') }}',
-                    link:  originLink
-                },
-                success: function copy(data) {
-                    if (data != null) {
-                      flag = false
-                        result = JSON.parse(data);
-                        $("#originLink").val(result.message);
-			$("#originLink").on('keydown',function(){
-				$("#shorten").text("SHORTEN");
-			})
-                        $("#shorten").text("Copy");
-                        $("#shorten").on('click', function(){
-                            if($("#originLink").val() !== "" && $("#shorten").text() == "Copy") {
-                                var shortenLink = $("#originLink").val();
-                                $("#originLink").select();
-                                document.execCommand("Copy");
-                                $.bootstrapGrowl("<strong>Copied!</strong>", {
-                                    ele: 'body',
-                                    type: 'success',
-                                    width: 'auto',
-                                    delay: 2000,
-                                    allow_dismiss: false
-                                });
-                            }
-                        });
-                    }else {
-                        $("#originLink").val("");
-                        $("#shorten").text("Shorten");
-			$.bootstrapGrowl("<strong>Error: Invalid Url !</strong>", {
-                                    ele: 'body',
-                                    type: 'error',
-                                    width: 'auto',
-                                    delay: 2000,
-                                    allow_dismiss: false
-                        });
-                    }
-                }
-            });
-        }
-    </script>
+    <script type="text/javascript" src="{{asset('js/shortenUrl.js')}}"></script>
 </html>
 
